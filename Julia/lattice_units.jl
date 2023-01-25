6d9f78f66bffb0de85aeeec0d57fb44caf597c3e
Anubhab Haldar <ahaldar@bu.edu>
2023-01-25 09:57:25 -0500

    Update lattice_units.jl

--- Julia/lattice_units.jl
+++ Julia/lattice_units.jl
@@ -1,8 +1,14 @@
 module LatticeUnits
 using Unitful; 
 using PhysicalConstants.CODATA2018; 
-@unit phe "phe" PhononEnergy (PlanckConstant / (2pi))^2 / ((0.1u"nm")^2 * AtomicMassConstant) false;
+
+length_scale = 0.1u"nm"
+mass_scale = 
+@unit phe "phe" PhononEnergy (PlanckConstant / (2pi))^2 / ((length_scale)^2 * AtomicMassConstant) false;
+@unit phf "phf" PhononEfield phe / (ElementaryCharge * length_scale) false;
 end
 
 Unitful.register(LatticeUnits)
+
 print(u"eV"(1.0u"phe"))
+print(u"V/m"(1.0u"phf"))
